c3cd790dc9a3ecb3e13fa7577bb828bb77c17fca
IlliaNebrat <illia.nebrat@gmail.com>
2023-10-16 15:14:50 +0300

    update class Rugby:fix variables, add methods.

--- src/illia/nebrat/homework/rugby/Rugby.java
+++ src/illia/nebrat/homework/rugby/Rugby.java
@@ -4,31 +4,39 @@ import java.util.Random;
 
 public class Rugby {
     public static void main(String[] args) {
+        int[] firstTeamAges = new int[25];
+        int[] secondTeamAges = new int[25];
+
+        int firstTeamTotalAge = fillArrayRandomAges(firstTeamAges);
+        int secondTeamTotalAge = fillArrayRandomAges(secondTeamAges);
+
+        System.out.println("Вік гравців першої команди:");
+        printArray(firstTeamAges);
+        System.out.println("\n\nВік гравців другої команди:");
+        printArray(secondTeamAges);
+
+        double firstTeamAverageAge = (double) firstTeamTotalAge / firstTeamAges.length;
+        double secondTeamAverageAge = (double) secondTeamTotalAge / secondTeamAges.length;
+
+        System.out.println("\n\nСередній вік гравців першої команди: " + firstTeamAverageAge);
+        System.out.println("Середній вік гравців другої команди: " + secondTeamAverageAge);
+    }
+
+    public static int fillArrayRandomAges(int[] array) {
         Random random = new Random();
-        int[] team1Ages = new int[25];
-        int[] team2Ages = new int[25];
+        int totalAge = 0;
 
-        int team1TotalAge = 0;
-        int team2TotalAge = 0;
-
-        System.out.println("Age of first team players:");
-        for (int i = 0; i < 25; i++) {
-            team1Ages[i] = random.nextInt(23) + 18;
-            System.out.print(team1Ages[i] + " ");
-            team1TotalAge += team1Ages[i];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(23) + 18;
+            totalAge += array[i];
         }
 
-        System.out.println("\n"+"Age of second team players:");
-        for (int i = 0; i < 25; i++) {
-            team2Ages[i] = random.nextInt(23) + 18;
-            System.out.print(team2Ages[i] + " ");
-            team2TotalAge += team2Ages[i];
+        return totalAge;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
         }
-
-        double team1AverageAge = (double) team1TotalAge / 25;
-        double team2AverageAge = (double) team2TotalAge / 25;
-
-        System.out.println("\n\n"+"Average age of first team players: " + team1AverageAge);
-        System.out.println("\n"+"Average age of first team players: " + team2AverageAge);
     }
 }
